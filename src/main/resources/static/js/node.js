184a8514ef50213a6d1825839bce5da504e1e801
zhangyancheng <zhangyancheng@jd.com>
2019-01-07 19:38:49 +0800

    重建root标签，避免点击后留下下划线样式

--- src/main/resources/static/js/node.js
+++ src/main/resources/static/js/node.js
@@ -1,19 +1,17 @@
 $(function () {
-    $("ul").delegate("li a", "click", function (e) {
-        e.preventDefault();
-        var href = this.id;
+    function getNodeInfo(href) {
         $.get("/info" + href, function (result) {
-            $("ul.breadcrumb li:gt(0)").remove();
+            var $breadcrumb = $("ul.breadcrumb");
+            $breadcrumb.empty().append("<li><a href='#' id='/'><span>/</span></a></li>");
 
             var isRoot = href == '/';
             if (!isRoot) {
                 $.each(href.split("/"), function (i, v) {
                     if (v != "") {
-                        $(".breadcrumb").append("<li><a href='#' id='" + href.substr(0, href.indexOf(v) + v.length) + "'><span>" + v + "</span></a></li>");
+                        $breadcrumb.append("<li><a href='#' id='" + href.substr(0, href.indexOf(v) + v.length) + "'><span>" + v + "</span></a></li>");
                     }
                 });
             }
-
             $("ul.children li:gt(0)").remove();
             $.each(result.children, function (i, v) {
                 $("ul.children").append("<li><a href='#' id='"+(isRoot ? href + v : href + '/' + v)+"'><span>" + v + "</span></a></li>");
@@ -25,6 +23,12 @@ $(function () {
             });
             $("#data").append(result.data);
         })
+    }
+
+    $("ul").delegate("li a", "click", function (e) {
+        e.preventDefault();
+        getNodeInfo(this.id);
     });
-    $(".breadcrumb li:first a").trigger("click");
+
+    getNodeInfo('/');
 });
