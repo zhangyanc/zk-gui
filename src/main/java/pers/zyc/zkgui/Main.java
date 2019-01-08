c2473f2cf9912ef8c8a5564a8048810fca98fba9
zhangyancheng <zhangyancheng@jd.com>
2019-01-08 21:10:28 +0800

    去session, 只支持frame

--- src/main/java/pers/zyc/zkgui/Main.java
+++ src/main/java/pers/zyc/zkgui/Main.java
@@ -11,8 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -20,17 +20,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.HandlerExceptionResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 import pers.zyc.tools.utils.Regex;
 import pers.zyc.tools.zkclient.ZKClient;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
@@ -41,6 +38,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -50,18 +48,59 @@ import java.util.stream.Stream;
  * @author zhangyancheng
  */
 @Controller
-@SpringBootApplication(exclude = {
-		ErrorMvcAutoConfiguration.class,
-		MultipartAutoConfiguration.class
-})
-public class Main extends JFrame implements ApplicationListener<EmbeddedServletContainerInitializedEvent> {
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class, MultipartAutoConfiguration.class})
+public class Main {
 
 	private static final int WEB_SERVER_PORT = 8090;
 	private static final String HISTORY_FILE = System.getProperty("user.home") + "/.zkgui_history";
 	private static final List<String> HISTORY_SET = new CopyOnWriteArrayList<>();
+	private static JFrame frame;
+	private static ZKClient zkClient;
 
 	public static void main(String[] args) throws Exception {
 		authIfNeed();
+		loadHistory();
+		SpringApplication app = new SpringApplication(Main.class);
+		app.setHeadless(false);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.setDefaultProperties(new HashMap<String, Object>() {
+			{
+				put("server.port", WEB_SERVER_PORT);
+				put("server.tomcat.max-threads", 2);
+				put("server.session.timeout", -1);
+				put("spring.thymeleaf.mode", "HTML5");
+				put("spring.thymeleaf.encoding", "UTF-8");
+				put("spring.thymeleaf.content-type", "text/html");
+				put("spring.thymeleaf.cache", false);
+				put("spring.thymeleaf.prefix", "classpath:/templates/");
+				put("spring.thymeleaf.suffix", ".htm");
+				put("spring.resources.static-locations", "classpath:/static");
+				put("spring.mvc.static-path-pattern", "/static/**");
+				put("spring.mvc.throw-exception-if-no-handler-found", true);
+
+				put("spring.jmx.enabled", false);
+				put("spring.mvc.formcontent.putfilter.enabled", false);
+			}
+		});
+		app.addListeners((EmbeddedServletContainerInitializedEvent e) -> {
+			Toolkit toolkit = Toolkit.getDefaultToolkit();
+			Dimension screenSize = toolkit.getScreenSize();
+			frame = new JFrame();
+			frame.setSize((int) (screenSize.width * 0.625), (int) (screenSize.height * 0.75));
+			frame.setLocationRelativeTo(null);
+			frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+			frame.setIconImage(toolkit.createImage(Main.class.getResource("/static/logo.png")));
+
+			Browser browser = new Browser();
+			frame.add(new BrowserView(browser), BorderLayout.CENTER);
+			frame.setVisible(true);
+			browser.loadURL("http://localhost:" + WEB_SERVER_PORT);
+		});
+		app.addListeners((ContextClosedEvent e) -> Optional.ofNullable(zkClient).ifPresent(ZKClient::destroy));
+		app.run(args);
+	}
+
+	private static void loadHistory() {
 		new Thread(() -> {
 			try {
 				File file = new File(HISTORY_FILE);
@@ -89,29 +128,6 @@ public class Main extends JFrame implements ApplicationListener<EmbeddedServletC
 			} catch (Exception ignored) {
 			}
 		}) {{setDaemon(true);}}.start();
-		SpringApplication app = new SpringApplication(Main.class);
-		app.setHeadless(false);
-		app.setBannerMode(Banner.Mode.OFF);
-		app.setDefaultProperties(new HashMap<String, Object>() {
-			{
-				put("server.port", WEB_SERVER_PORT);
-				put("server.tomcat.max-threads", 2);
-				put("server.session.timeout", -1);
-				put("spring.thymeleaf.mode", "HTML5");
-				put("spring.thymeleaf.encoding", "UTF-8");
-				put("spring.thymeleaf.content-type", "text/html");
-				put("spring.thymeleaf.cache", false);
-				put("spring.thymeleaf.prefix", "classpath:/templates/");
-				put("spring.thymeleaf.suffix", ".htm");
-				put("spring.resources.static-locations", "classpath:/static");
-				put("spring.mvc.static-path-pattern", "/static/**");
-				put("spring.mvc.throw-exception-if-no-handler-found", true);
-
-				put("spring.jmx.enabled", false);
-				put("spring.mvc.formcontent.putfilter.enabled", false);
-			}
-		});
-		app.run(args);
 	}
 
 	private static void authIfNeed() throws Exception {
@@ -138,35 +154,14 @@ public class Main extends JFrame implements ApplicationListener<EmbeddedServletC
 		}
 	}
 
-	@Override
-	public void onApplicationEvent(EmbeddedServletContainerInitializedEvent event) {
-		Toolkit toolkit = Toolkit.getDefaultToolkit();
-		Dimension screenSize = toolkit.getScreenSize();
-		setSize((int) (screenSize.width * 0.625), (int) (screenSize.height * 0.75));
-		setResizable(false);
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		setIconImage(toolkit.createImage(Main.class.getResource("/static/logo.png")));
-
-		Browser browser = new Browser();
-		add(new BrowserView(browser), BorderLayout.CENTER);
-		setVisible(true);
-		browser.loadURL("http://localhost:" + WEB_SERVER_PORT);
-	}
-
 	@RequestMapping(path = "/", method = RequestMethod.GET)
-	public String connect(HttpSession session, ModelMap modelMap) {
-		ZKClient zkClient = (ZKClient) session.getAttribute("ZK_CLIENT");
-		if (zkClient != null) {
-			return "redirect:/node";
-		}
+	public String connect(ModelMap modelMap) {
 		modelMap.put("history", HISTORY_SET);
 		return "connect";
 	}
 
 	@RequestMapping(path = "/", method = RequestMethod.POST)
-	public void connect(HttpServletRequest request, HttpServletResponse response,
-						  String connectString) throws Exception {
+	public void connect(HttpServletResponse response, String connectString) throws Exception {
 		if (!Regex.ZK_ADDRESS.matches(connectString)) {
 			throw new RuntimeException("Invalid connect string: " + connectString);
 		}
@@ -176,20 +171,16 @@ public class Main extends JFrame implements ApplicationListener<EmbeddedServletC
 			zkClient.destroy();
 			throw new RuntimeException("Connect to " + connectString + " timeout");
 		}
-		request.getSession().setAttribute("ZK_CLIENT", zkClient);
+		Main.zkClient = zkClient;
 		response.sendRedirect("/node");
 		HISTORY_SET.removeIf(connectString::equals);
 		HISTORY_SET.add(0, connectString);
 	}
 
 	@RequestMapping("/quit")
-	public void quit(HttpSession session, HttpServletResponse response) throws IOException {
-		ZKClient zkClient = (ZKClient) session.getAttribute("ZK_CLIENT");
-		if (zkClient != null) {
-			zkClient.destroy();
-		}
-		session.invalidate();
-		setTitle(null);
+	public void quit(HttpServletResponse response) throws IOException {
+		zkClient.destroy();
+		frame.setTitle(null);
 		response.sendRedirect("/");
 	}
 
@@ -200,8 +191,7 @@ public class Main extends JFrame implements ApplicationListener<EmbeddedServletC
 		if (StringUtils.isBlank(node)) {
 			node = "/";
 		}
-		setTitle(node);
-		ZKClient zkClient = (ZKClient) request.getSession().getAttribute("ZK_CLIENT");
+		frame.setTitle(node);
 		List<String> children = zkClient.getChildren(node);
 		Stat stat = new Stat();
 		byte[] data = zkClient.getData(node, stat);
@@ -215,23 +205,6 @@ public class Main extends JFrame implements ApplicationListener<EmbeddedServletC
 	@Configuration
 	@SuppressWarnings("unused")
 	static class MvcConfigurer extends WebMvcConfigurerAdapter {
-
-		@Override
-		public void addInterceptors(InterceptorRegistry registry) {
-			registry.addInterceptor(new HandlerInterceptorAdapter() {
-				@Override
-				public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
-										 Object handler) throws Exception {
-					HttpSession session = request.getSession();
-					ZKClient zkClient = (ZKClient) session.getAttribute("ZK_CLIENT");
-					if (zkClient == null) {
-						response.sendRedirect("/");
-						return false;
-					}
-					return true;
-				}
-			}).addPathPatterns("/node", "/info/**");
-		}
 
 		@Override
 		public void addViewControllers(ViewControllerRegistry registry) {
